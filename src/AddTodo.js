6a17ddd126f66f47bfc1c61217afcac7505fc045
Oleg5107767 <github.com/Oleg5107767/my-test-.git>
2022-09-20 19:15:37 +0300

    first

---
+++ src/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, Button, Alert} from 'react-native';
+
+export const AddTodo = ({ onSumbit }) => {
+    const [value, setValue] = useState('')
+    const pressHandler = () => {
+        if(value.trim()){
+            onSumbit(value)
+            setValue('')
+        }else{
+            //error
+            Alert.alert('Название дела не может быть пустым')
+        }
+    }
+    
+    return (
+        <View style={styles.wraper}>
+            <TextInput 
+                style={styles.input} 
+                onChangeText={setValue}
+                value={value}
+                placeholder="Введите название дела..."
+                autoCorrect={false}
+                autoCapitalize='none'
+            />
+            <Button title='Добавить' onPress={pressHandler} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    wraper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 15
+    },
+    input: {
+        width: "70%",
+        padding: 10,
+        borderStyle: "solid",
+        borderBottomWidth: 2,
+        borderBottomColor: "#3949ab"
+    },
+})
+
